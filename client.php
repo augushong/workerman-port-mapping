efc98e130b087ff76be53a264590ee8b0783019f
augushong <augushong@163.com>
2018-09-30 14:55:18 +0800

    项目完成,可以穿透,已测试可穿透80,22.

--- client.php
+++ client.php
@@ -1,74 +1,90 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/common.php';
 
 use Workerman\Worker;
 use \Workerman\Connection\AsyncTcpConnection;
 
+
+try{
+    $config = get_config();
+}catch(\Exception $e){
+    echo "error:{$e}\n";
+}
+
+if(isset($config['nat_list']) && !is_win()){
+    foreach ($config['nat_list'] as $n_key => $n_value) {
+        $unique_key = $n_key;
+        $nat_client_list['nat_client_worker_'.$n_key] = build_client_woker($n_value);
+    }
+}else{
+    $worker = build_client_woker($config);
+}
+
+Worker::runAll();
+
+
+function build_client_woker($config){
+    
 $inside_worker = new Worker();
 
-
-$inside_worker->onWorkerStart = function() use ($inside_worker){
+$inside_worker->onWorkerStart = function() use ($inside_worker,$config){
 
     // Channel客户端连接到Channel服务端
-    Channel\Client::connect('47.88.58.78', 2206);
+    Channel\Client::connect($config['server_ip'], $config['channel_port']);
 
-    Channel\Client::on('cs_connect', function($event_data) use($inside_worker){
-        echo "cs_connect\n";
+    Channel\Client::on('cs_connect'.$config['local_ip'].":".$config['local_port'], function($event_data) use($inside_worker,$config){
 
-        $connection_to_local = new AsyncTcpConnection('tcp://127.0.0.1:80');
-
+        $local_host_name = "tcp://".$config['local_ip'].":".$config['local_port'];
         
-        $connection_to_local->onConnect = function($connection){
-            // $connect_data['session'] = $_SESSION;
+        $connection_to_local = new AsyncTcpConnection($local_host_name);
+
+        $connection_to_local->onConnect = function($connection) use ($event_data,$config){
+
             $connect_data['connection'] = [
                 'ip'=>$connection->getRemoteIp(),
-                'port'=>$connection->getRemotePort()
+                'port'=>$connection->getRemotePort(),
+                'c_connection_id'=>$event_data['connection']['c_connection_id']
             ];
-            Channel\Client::publish('sc_connect',$connect_data);
-            echo "sc_connect\n";
+            Channel\Client::publish('sc_connect'.$config['local_ip'].":".$config['local_port'],$connect_data);
         };
 
-        $connection_to_local->onMessage = function($connection,$data){
+        $connection_to_local->onMessage = function($connection,$data) use($config,$event_data){
             // $message_data['session'] = $_SESSION;
             $message_data['data'] = $data;
             $message_data['connection'] = [
                 'ip'=>$connection->getRemoteIp(),
-                'port'=>$connection->getRemotePort()
+                'port'=>$connection->getRemotePort(),
+                'c_connection_id'=>$event_data['connection']['c_connection_id']
             ];
 
-            Channel\Client::publish('sc_message',$message_data);
-            echo "sc message\n";
+            Channel\Client::publish('sc_message'.$config['local_ip'].":".$config['local_port'],$message_data);
         };
 
-        $connection_to_local->onClose = function($connection){
+        $connection_to_local->onClose = function($connection) use($event_data,$config){
             // $close_data['session'] = $_SESSION;
             $close_data['connection'] = [
                 'ip'=>$connection->getRemoteIp(),
-                'port'=>$connection->getRemotePort()
+                'port'=>$connection->getRemotePort(),
+                'c_connection_id'=>$event_data['connection']['c_connection_id']
             ];
             
-            Channel\Client::publish('sc_close',$close_data);
-            echo "sc close\n";
+            Channel\Client::publish('sc_close'.$config['local_ip'].":".$config['local_port'],$close_data);
         };
         
         $connection_to_local->connect();
 
-
-        $inside_worker->connections[$event_data['connection']['ip'].$event_data['connection']['port']] = $connection_to_local;
+        $inside_worker->connections[$event_data['connection']['c_connection_id']] = $connection_to_local;
         
     });
-    Channel\Client::on('cs_message',function($event_data)use($inside_worker){
-        echo "cs_message\n";
-        $inside_worker->connections[$event_data['connection']['ip'].$event_data['connection']['port']]->send($event_data['data']);
+
+    Channel\Client::on('cs_message'.$config['local_ip'].":".$config['local_port'],function($event_data)use($inside_worker){
+        $inside_worker->connections[$event_data['connection']['c_connection_id']]->send($event_data['data']);
     });
-    Channel\Client::on('cs_close',function($event_data)use($inside_worker){
-        echo "cs_close\n";
-        $inside_worker->connections[$event_data['connection']['ip'].$event_data['connection']['port']]->close();
+    Channel\Client::on('cs_close'.$config['local_ip'].":".$config['local_port'],function($event_data)use($inside_worker){
+        $inside_worker->connections[$event_data['connection']['c_connection_id']]->close();
     });
 };
 
 
-if(!defined('GLOBAL_START'))
-{
-    Worker::runAll();
 }
--- server.php
+++ server.php
@@ -1,73 +1,90 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/common.php';
 
 use Workerman\Worker;
 
 $channel_server = new Channel\Server('0.0.0.0', 2206);
 
-$outside_worker = new Worker('tcp://0.0.0.0:888');
 
-$outside_worker->onWorkerStart = function(){
-    // Channel客户端连接到Channel服务端
-    Channel\Client::connect('127.0.0.1', 2206);
-
-};
-
-$outside_worker->onConnect = function($connection){
-
-    // $connection_data['session'] = $_SESSION;
-    $connection_data['connection'] = [
-        'ip'=>$connection->getRemoteIp(),
-        'port'=>$connection->getRemotePort()
-    ];
-
-
-    Channel\Client::publish('cs_connect', $connection_data);
-    echo "cs_connect\n";
-
-    Channel\Client::on('sc_message',function($event_data) use ($connection){
-        echo "sc_message\n";
-        var_dump($event_data['data']);
-        $connection->send($event_data['data']);
-    });
-    Channel\Client::on('sc_close',function($event_data) use ($connection){
-        echo "sc_close\n";
-        $connection->close();
-    });
-    Channel\Client::on('sc_connect',function($event_data) use($connection){
-        echo "sc_connect\n";
-
-    });
-    
-    $connection->onMessage = function($connection, $data){
-        // $message_data['session'] = $_SESSION;
-        $message_data['connection'] = [
-            'ip'=>$connection->getRemoteIp(),
-            'port'=>$connection->getRemotePort()
-        ];
-        $message_data['data'] = $data;
-
-        Channel\Client::publish('cs_message', $message_data);
-        echo "cs_message\n";
-    };
-    
-    $connection->onClose = function ($connection){
-    
-        // $close_data['session'] = $_SESSION;
-        $close_data['connection'] = [
-            'ip'=>$connection->getRemoteIp(),
-            'port'=>$connection->getRemotePort()
-        ];
-    
-        Channel\Client::publish('cs_close', $close_data);
-        echo "cs_close\n";
-    };
-
-};
-
-
-
-if(!defined('GLOBAL_START'))
-{
-    Worker::runAll();
+try{
+    $config = get_config();
+}catch(\Exception $e){
+    echo "error:{$e}\n";
 }
+
+if(isset($config['nat_list']) && !is_win()){
+    foreach ($config['nat_list'] as $n_key => $n_value) {
+        $unique_key = $n_key;
+        $nat_client_list['nat_client_worker_'.$n_key] = build_server_woker($n_value);
+    }
+}else{
+    $worker = build_server_woker($config);
+}
+
+Worker::runAll();
+
+
+function build_server_woker($config){
+
+    
+    $outside_worker = new Worker('tcp://0.0.0.0:'.$config['server_port']);
+    
+    $outside_worker->onWorkerStart = function() use ($outside_worker,$config){
+        // Channel客户端连接到Channel服务端
+        Channel\Client::connect('127.0.0.1', $config['channel_port']);
+            
+        Channel\Client::on('sc_message'.$config['local_ip'].":".$config['local_port'],function($event_data) use ($outside_worker){
+            $outside_worker->connections[$event_data['connection']['c_connection_id']]->send($event_data['data']);
+        });
+
+        Channel\Client::on('sc_close'.$config['local_ip'].":".$config['local_port'],function($event_data) use ($outside_worker){
+            $outside_worker->connections[$event_data['connection']['c_connection_id']]->close();
+        });
+
+        Channel\Client::on('sc_connect'.$config['local_ip'].":".$config['local_port'],function($event_data) use($outside_worker){
+
+        });
+
+    };
+    
+    $outside_worker->onConnect = function($connection) use ($config){
+    
+        // $connection_data['session'] = $_SESSION;
+        $connection_data['connection'] = [
+            'ip'=>$connection->getRemoteIp(),
+            'port'=>$connection->getRemotePort(),
+            'c_connection_id'=>$connection->id
+        ];
+    
+    
+        Channel\Client::publish('cs_connect'.$config['local_ip'].":".$config['local_port'], $connection_data);
+        
+        $connection->onMessage = function($connection, $data) use ($config){
+            // $message_data['session'] = $_SESSION;
+            $message_data['connection'] = [
+                'ip'=>$connection->getRemoteIp(),
+                'port'=>$connection->getRemotePort(),
+                'c_connection_id'=>$connection->id
+            ];
+            $message_data['data'] = $data;
+    
+            Channel\Client::publish('cs_message'.$config['local_ip'].":".$config['local_port'], $message_data);
+            
+        };
+        
+        $connection->onClose = function ($connection) use ($config){
+        
+            // $close_data['session'] = $_SESSION;
+            $close_data['connection'] = [
+                'ip'=>$connection->getRemoteIp(),
+                'port'=>$connection->getRemotePort(),
+                'c_connection_id'=>$connection->id
+            ];
+        
+            Channel\Client::publish('cs_close'.$config['local_ip'].":".$config['local_port'], $close_data);
+        };
+    };
+
+}
+
